f625bb857598c3de1cf6a7033427e89f761c92ae
Daren Warburton <daren.sdw@gmail.com>
2015-06-05 22:39:18 +0100

    minor changes

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
   // When running Ghost in the wild, use the production environment
   // Configure your URL and mail settings here
   production: {
-    url: 'http://docgecko.com',
+    url: process.env.BLOG_URL,
 
     mail: {
       transport: 'SMTP',
